f049abb0c9c22dca04037dafce54ea64e93b0c07
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-02-09 23:51:23 +0300

    updt

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 baguetteBox.run('.gallery');
 const search = new Filter('search', 'data-caption');
+
+// const remove = document.querySelector('btn-remove');
+
+// remove.addEventListener(`click`, () =>{
+
+// const lastChild = document.querySelector(':last-child');
+//   lastChild.remove();
+
+// })
